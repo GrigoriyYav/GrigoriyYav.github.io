89e1f1ff7a9b646f5de208d6274a4fb12686c647
GrigoriyYav <yavgan06@gmail.com>
2021-07-27 16:41:50 +0400

    feat: add tabs and made script for switching

---
+++ script.js
@@ -0,0 +1,62 @@
+var $tabs = function (target) {
+    var
+      _elemTabs = (typeof target === 'string' ? document.querySelector(target) : target),
+      _eventTabsShow,
+      _showTab = function (tabsLinkTarget) {
+        var tabsPaneTarget, tabsLinkActive, tabsPaneShow;
+        tabsPaneTarget = document.querySelector(tabsLinkTarget.getAttribute('href'));
+        tabsLinkActive = tabsLinkTarget.parentElement.querySelector('.tabs_link_active');
+        tabsPaneShow = tabsPaneTarget.parentElement.querySelector('.tabs_pane_show');
+        // если следующая вкладка равна активной, то завершаем работу
+        if (tabsLinkTarget === tabsLinkActive) {
+          return;
+        }
+        // удаляем классы у текущих активных элементов
+        if (tabsLinkActive !== null) {
+          tabsLinkActive.classList.remove('tabs_link_active');
+        }
+        if (tabsPaneShow !== null) {
+          tabsPaneShow.classList.remove('tabs_pane_show');
+        }
+        // добавляем классы к элементам (в завимости от выбранной вкладки)
+        tabsLinkTarget.classList.add('tabs_link_active');
+        tabsPaneTarget.classList.add('tabs_pane_show');
+        document.dispatchEvent(_eventTabsShow);
+      },
+      _switchTabTo = function (tabsLinkIndex) {
+        var tabsLinks = _elemTabs.querySelectorAll('.tabs_link');
+        if (tabsLinks.length > 0) {
+          if (tabsLinkIndex > tabsLinks.length) {
+            tabsLinkIndex = tabsLinks.length;
+          } else if (tabsLinkIndex < 1) {
+            tabsLinkIndex = 1;
+          }
+          _showTab(tabsLinks[tabsLinkIndex - 1]);
+        }
+      };
+    
+    _eventTabsShow = new CustomEvent('tab.show', { detail: _elemTabs });
+    
+    _elemTabs.addEventListener('click', function (e) {
+      var tabsLinkTarget = e.target;
+      // завершаем выполнение функции, если кликнули не по ссылке
+      if (!tabsLinkTarget.classList.contains('tabs_link')) {
+        return;
+      }
+      // отменяем стандартное действие
+      e.preventDefault();
+      _showTab(tabsLinkTarget);
+    });
+    
+    return {
+      showTab: function (target) {
+        _showTab(target);
+      },
+      switchTabTo: function (index) {
+        _switchTabTo(index);
+      }
+    }
+    
+    };
+    
+    $tabs('.tabs');
